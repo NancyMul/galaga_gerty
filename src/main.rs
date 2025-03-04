74e5c8615268a182c7931bd0a09ae4a13b60f593
NancyMul <nancymuller751@gmail.com>
2025-03-04 10:44:25 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,31 +19,27 @@ pub const ROWS: usize = 10;
 pub const COLUMNS: usize = 20;
 
 pub struct GameState {
-    pub game_board: HashMap<Coords, char>, // 'Char' should be lowercase 'char'
+    pub game_board: HashMap<Coords, char>,
     pub player_position: Coords
 }
-
-// ACHTUNG!
-// I changed Cords to the correct spelling Coords
-// John is primitive rock child
 
 // To run your program
     // cargo run
 
 // When you finish tasks and your code compiles with no errors
     // git add .
-    // git commit -m "nancy is the best"
+    // git commit -m "nancy is still the best"
     // git push
 
 impl GameState{
-    pub fn new() -> GameState{ // We are not calling a function, remove the parentheses after GameState
+    pub fn new() -> GameState { 
         GameState{
             game_board: HashMap::new(),
             player_position: Coords(10, 8),
         }
     }
 
-    pub fn add_ship(&mut self, coords: Coords, ship: char) { // 'Char' should be lowercase 'char'
+    pub fn add_ship(&mut self, coords: Coords, ship: char) { 
         self.game_board.insert(coords, ship);  
     }
 
@@ -59,7 +55,7 @@ impl GameState{
         for row in 0..ROWS {
             print!("           |");
             for col in 0..COLUMNS {
-            let current_position = Coords(col, row);
+                let current_position = Coords(col, row);
                 if self.player_position == current_position {
                     print!("^");
                 } else if let Some(ship) = self.game_board.get(&current_position) {
@@ -79,10 +75,14 @@ impl GameState{
     }
 
     pub async fn run_game(&mut self) { 
-    
         loop {
             self.display_board();
-            self.player_position = on_press(self.player_position).await;
+            self.player_position = on_press(self.player_position).await; // delete this line, we will rewrite it differently below
+
+            // use an if let statement to check if self.player.use_key().await returned Some
+            // if so, run self.add_ship()
+            // the first parameter of add_ship() should be the coordinates that were returned in the if statement
+            // the second parameter should be a '|' this will represent a bullet
         }
     }
 }
@@ -92,7 +92,7 @@ pub async fn main() {
     execute!(std::io::stdout(), terminal::Clear(terminal::ClearType::All), cursor::MoveTo(0, 0));
 
     let mut game = GameState::new();
-    game.add_ship(Coords(8, 4), 'F');
+    game.add_ship(Coords(10, 4), 'F');
 
     game.run_game().await;
 }
@@ -124,21 +124,50 @@ impl KeyReader {
     }
 }
 
+pub struct Player {
+    pub display_char: char,
+    pub current_position: Option<Coords>,
+    // pub death_timer: Timer, 
+    pub key_reader: KeyReader,
+}
+
+// Create an impl block for Player
+// Add a new() function
+// This function should return Self
+// Return an instance of Player
+// where display character is '^'
+// set current_position to Coords(COLUMNS / 2, ROWS - 2);
+// death_timer to Timer::new(200),
+// and key_reader KeyReader::new(),
 
 
-pub async fn on_press(mut player_position: Coords)-> Coords {
+// move this function into a impl block for the Player struct
+// change this so it takes in &mut self instead of player_position
+// rename to use_key
+pub async fn on_press(mut player_position: Coords) -> Coords { // change to Option<Coords>
     match KeyReader::await_key_press().await {
         Key::ArrowLeft => {
-            player_position.0 -= 1;
+            player_position.0 -= 1; // change to self.current_position.0
         }
         Key::ArrowRight => {
-            player_position.0 += 1;
+            player_position.0 += 1; // change to self.current_position.0
         }
         Key::ArrowUp => {
+            // ArrowUp is the key that makes the player shoot
+            // This function now returns an Option<Coords>
+            // Meaning if we return Some(Coords(x, y)) then the player shot a bullet from that position
+            // If we return None, the player did not shoot
+            // Here, the player shot, so let's return the player's current position
+            // Return Some(self.current_position.0)
         }
         Key::CtrlC => exit(0),
         _ => {}
     }
+    // When we change self.current_position.0 we directly change the position of the player
+    // We do not need to return the player_position anymore
+    // Instead we will return a set of coordinates, only when the player shoots
+    // That return will happen above in the ArrowUp match arm
+    // Return None here instead of player_position
     player_position
 }
 
@@ -183,11 +212,4 @@ pub enum Ship {
     Bullet(ShipAI, bool, Uuid),
 }
 
-pub struct Player {
-    pub display_char: char,
-    pub lives: u8,
-    pub current_position: Option<Coords>,
-    pub start_position: Coords,
-    // pub death_timer: Timer, 
-    pub key_reader: KeyReader,
-}
+
